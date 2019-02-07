251c290c145f49880ef64d1ce29de69f263a94d0
VasanthaKumar <vasanthakumar@LAPSZ3359.ird.mu-sigma.com>
2019-02-07 22:07:35 +0530

    passing data to stfl thru initstate

--- intro/lib/main.dart
+++ intro/lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text("Easy list"),
         ),
-        body: ProductManager(),
+        body: ProductManager('Food Tester'),
       ),
     );
   }
