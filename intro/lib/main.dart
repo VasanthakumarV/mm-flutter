1391dcf92099095dad279a334bcc48ed8008a212
VasanthaKumar <vasanthakumar@LAPSZ3359.ird.mu-sigma.com>
2019-02-07 19:18:18 +0530

    card

--- intro/lib/main.dart
+++ intro/lib/main.dart
@@ -10,6 +10,14 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text("Easy list"),
         ),
+        body: Card(
+          child: Column(
+            children: <Widget>[
+              Image.asset("assets/food.jpg"),
+              Text("Food paradise"),
+            ],
+          ),
+        ),
       ),
     );
   }
