4767fea940405454bfc7e4f50ecd10f7ffd163c6
VasanthakumarV <vasanth260m12@gmail.com>
2019-03-08 10:25:32 +0530

    dialog and notfound route

--- navigation/lib/main.dart
+++ navigation/lib/main.dart
@@ -58,6 +58,11 @@ class _MyAppState extends State<MyApp> {
         }
         return null;
       },
+      onUnknownRoute: (RouteSettings settings) {
+        return MaterialPageRoute(
+            builder: (BuildContext context) =>
+                ProductsPage(_products, _addProducts, _deleteProduct));
+      },
     );
   }
 }
